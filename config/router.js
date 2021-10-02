e6eb3c40b0896a074514028409e775a73237c5e9
James Gore <jamesgore@Jamess-MacBook-Pro.local>
2021-10-02 17:08:01 +0100

    working controller & routes

--- config/router.js
+++ config/router.js
@@ -1,13 +1,18 @@
 import express from 'express'
 import countriesController from '../controllers/countriesController.js'
 
-
 // Router is reponsible for which routes are answered by which controllers
 const Router = express.Router()
 
 // base URL of this is determined by api
 // here we handle everything that comes after that base
-Router.route('/countries').get(countriesController.getAllCountries)
+Router.route('/countries')
+  .get(countriesController.getAllCountries)
+  .post(countriesController.createCountry)
+
+Router.route('/countries/:id')
+  .get(countriesController.getCountry)
+  .delete(countriesController.deleteCountry)
 
 export default Router
 
--- controllers/countriesController.js
+++ controllers/countriesController.js
@@ -10,6 +10,57 @@ async function getAllCountries(_req, res, next) {
     next(err)
   };
 }
-//update
 
-export default { getAllCountries }
+//create Country
+async function createCountry(req, res, next) {
+  try {
+    const newCountry = await Country.create({...req.body})
+    
+    return res.status(201).json(newCountry)
+  }catch(err) {
+    next(err)
+  }
+}
+
+//get country
+async function getCountry(req, res, next) {
+  const id = req.params.id
+  try {
+    const country = await Country.findById(id)
+    
+    if (!country) {
+      return res.status(404).send({message: 'Country does not exit'})
+    }
+    return res.status(200).json(country)
+  } catch (err) {
+    next(err)
+  }
+};
+
+//delete country
+async function deleteCountry(req, res, next) {
+  try {
+    const id = req.params.id
+    
+    const country = await Country.findById(id)
+    
+    if (!country) {
+      return res.status(404).send({ message: 'Country does not exist'})
+    }
+
+    await country.remove()
+    return res.status(200).json(country).send({ message: `${country} was deleted`})
+    
+  } catch (err) {
+    next (err)
+  }
+}
+
+//update country
+
+
+
+
+
+export default { getAllCountries, getCountry, createCountry, deleteCountry }
+
