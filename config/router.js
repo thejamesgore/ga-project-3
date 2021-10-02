53245800d8fa5606a857f3c9194d60e85a446c0e
James Gore <jamesgore@Jamess-MacBook-Pro.local>
2021-10-02 16:03:42 +0100

    working api

--- config/router.js
+++ config/router.js
@@ -1,5 +1,5 @@
 import express from 'express'
-import { getAllCountries } from '../controllers/countriesController.js'
+import countriesController from '../controllers/countriesController.js'
 
 
 // Router is reponsible for which routes are answered by which controllers
@@ -7,7 +7,7 @@ const Router = express.Router()
 
 // base URL of this is determined by api
 // here we handle everything that comes after that base
-Router.route('/countries').get(getAllCountries)
+Router.route('/countries').get(countriesController.getAllCountries)
 
 export default Router
 
--- controllers/countriesController.js
+++ controllers/countriesController.js
@@ -1,10 +1,8 @@
-// How we get the places(from database or otherwise)
-
-import Country from "../models/country";
+import Country from "../models/country.js";
 
 // is the responsibility of the controller
 
-async function getAllCountries(req, res, next) {
+async function getAllCountries(_req, res, next) {
   try {
     const countries = await Country.find()
     return res.status(200).json(countries)
