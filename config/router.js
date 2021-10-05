27cf7714cdcfe3b404b7dabbc5aeffd39a19f463
Oliver Pettyfer <oliverpettyfer@Olivers-MacBook-Pro.local>
2021-10-05 21:12:30 +0100

    updated Route

--- config/router.js
+++ config/router.js
@@ -2,6 +2,7 @@ import express from "express";
 import countriesController from "../controllers/countriesController.js";
 import commentsController from "../controllers/commentsController.js";
 import citiesController from "../controllers/citiesController.js";
+import userController from "../contorllers/userController.js";
 
 // Router is reponsible for which routes are answered by which controllers
 const Router = express.Router();
@@ -22,12 +23,14 @@ Router.route("/countries/:id/comments").post(commentsController.createComment);
 Router.route("/countries/:id/comments/:commentId")
   .put(commentsController.updateComment)
   .delete(commentsController.deleteComment);
-  
-Router.route("/cities").post(citiesController.createCity)
 
-Router.route('/cities/:id').delete(citiesController.deleteCity)
-  
-  
+Router.route("/cities").post(citiesController.createCity);
+
+Router.route("/cities/:id").delete(citiesController.deleteCity);
+
+Router.route("/reigster").post(userController.registerUser);
+Router.route("/login").post(userController.loginUser);
+
 export default Router;
 
 //update
