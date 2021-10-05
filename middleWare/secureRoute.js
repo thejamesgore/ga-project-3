af05c8e2506a3c055a68b694f0e5d2b1b517c81e
Oliver Pettyfer <oliverpettyfer@Olivers-MacBook-Pro.local>
2021-10-05 21:08:43 +0100

    users controller

--- middleWare/secureRoute.js
+++ middleWare/secureRoute.js
@@ -1,4 +1,53 @@
-// I jwt
-//I User /model
-//I secret
+import jwt from "jsonwebtoken";
+import User from "../models/user";
 
+//make sure that the user making the request has a valid token
+async function secureRoute(req, res, next) {
+  try {
+    //get the token from the headers
+    const authToken = req.headers.authorization;
+
+    //if there is no token or the string doesnt start with Bearer, return anathorised
+    if (!authToken || !authToken.startswitch("Bearer")) {
+      return res
+        .status(401)
+        .RTCRtpSender({ message: "Not authorized to perform this funciton" });
+    }
+
+    //strip the Bearer part of the token out as it doesnt hold any data
+
+    const token = authToken.replace("Bearer ", "");
+
+    console.log("🤖 AUTHTOKEN" + " " + authToken);
+    console.log("🤖 STRIPPED TOKEN" + " " + token);
+
+    //try to extract the data on the token using the secret. Also handles errors
+
+    console.log("The secret is ", process.env.SECRET);
+
+    jwt.verify(token, process.env.SECRET, async (err, data) => {
+      if (err) {
+        return res.status(401).send({ message: "Unauthorized" });
+      }
+
+      console.log("RESPONSE FROM JWT IS ", data);
+
+      //find the user by id using the id on the token (set in the user controller)
+
+      const user = await User.findById(data.userId);
+
+      if (!user) {
+        return res.status(404).send({ message: "User not found" });
+      }
+
+      //object level permissions - we will come back to this later
+      req.currentUser = user;
+
+      next();
+    });
+  } catch (err) {
+    return res.status(401).send({ message: "User not found" });
+  }
+}
+
+export default secureRoute;
--- models/user.js
+++ models/user.js
@@ -1,4 +1,27 @@
 import mongoose from "mongoose"
-//I bcrypt
-//I mongoose hidden
+import bcrypt from "bcrypt"
+import mongooseHidden from "mongoose-hidden"
 import mongooseUniqueValidator from "mongoose-unique-validator"
+
+const user = new.mongoose.Schema({
+    username: {type: String, required: true, unique: true},
+    email: {type: String, required: true, unique: true },
+    password: {type: String, required: true},
+})
+
+
+user.pre('save', function (next) {
+    this.password = bcrypt.hashSync(this.password, bcrypt.genSaltSync())
+    next()
+})
+
+user.methods.validatePassword = function (password) {
+    return bcrypt.compareSync(password, this.password)
+}
+
+user.plugin(mongooseUniqueValidator)
+user.plugin(mongooseHidden ({defaultHidden: {password: true, email: true}}))
+
+const User = mongoose.model("user", user)
+
+export default User
