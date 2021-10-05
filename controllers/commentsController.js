21c3caf6bbb01cee2d3b66627c8443960ae1eacb
James Gore <jamesgore@Jamess-MacBook-Pro.local>
2021-10-05 18:58:57 +0100

    updated commentsController

--- controllers/commentsController.js
+++ controllers/commentsController.js
@@ -33,6 +33,7 @@ async function deleteComment(req, res, next) {
     if (!country) {
       return res.status(404).send({ message: "country does not exist" });
     }
+    console.log(req.params)
     const comment = country.comments.id(commentId);
 
     if (!comment) {
@@ -53,22 +54,25 @@ async function updateComment(req, res, next) {
   try {
     const { id, commentId } = req.params;
     const country = await Country.findById(id);
-
+    console.log(req.params)
     if (!country) {
       return res.status(404).send({ message: "Country does not exist" });
     }
 
-    const comment = country.comments.id(commentId);
+    const comment = country.comments.id(commentId)
+    
     if (!comment) {
       return res.status(404).send({ message: "Comment does not exist" });
     }
 
     comment.set(req.body);
-    const savedCountry = await movie.save();
+    const savedCountry = await country.save();
     return res.status(200).send(savedCountry);
   } catch (err) {
     next(err);
   }
 }
+
+
 
 export default { createComment, deleteComment, updateComment };
