b8c20aa0159859dc3efc23e7ea93c874dc92a301
Oliver Pettyfer <oliverpettyfer@Olivers-MacBook-Pro.local>
2021-10-14 22:10:18 +0100

    updated seed data and schema

--- db/countrySeedData.js
+++ db/countrySeedData.js
@@ -1,27 +1,31 @@
 export const countrySeedData = [
   {
     name: "United Kingdom",
-    city: [],
+    city: "London",
     yearVisited: 2000,
-    comments: [],
+    comments: "wet",
+    rating: 2,
   },
 
   {
     name: "Malta",
-    city: [],
+    city: "Malta",
     yearVisited: 2000,
-    comments: [],
+    comments: "ok",
+    rating: 2,
   },
   {
     name: "Australia",
-    city: [],
+    city: "Perth",
     yearVisited: 2020,
-    comments: [],
+    comments: "amazing",
+    rating: 2,
   },
   {
     name: "North America",
-    city: [],
+    city: "new-york",
     yearVisited: 1999,
-    comments: [],
+    comments: "good",
+    rating: 2,
   },
 ];
--- models/country.js
+++ models/country.js
@@ -1,29 +1,21 @@
 import mongoose from "mongoose";
 import mongooseUniqueValidator from "mongoose-Unique-Validator";
 
-const commentSchema = new mongoose.Schema(
+const countriesSchema = new mongoose.Schema(
   {
-    text: { type: String, maxlength: 300 },
+    // userAccount: { type: String, required: true },
+    name: { type: String, required: true },
+    city: { type: String },
+    yearVisited: { type: Number, required: true },
+    comments: { type: String, maxlength: 300 },
     rating: { type: Number, min: 1, max: 5 },
+    createdBy: {
+      type: mongoose.Schema.ObjectId,
+      ref: "user",
+    },
   },
   { timestamp: true }
 );
-
-const citiesSchema = new mongoose.Schema({
-  name: { type: String },
-});
-
-const countriesSchema = new mongoose.Schema({
-  // userAccount: { type: String, required: true },
-  name: { type: String, required: true },
-  city: [citiesSchema],
-  yearVisited: { type: Number, required: true },
-  comments: [commentSchema],
-  createdBy: {
-    type: mongoose.Schema.ObjectId,
-    ref: "user",
-  },
-});
 
 countriesSchema.plugin(mongooseUniqueValidator);
 
